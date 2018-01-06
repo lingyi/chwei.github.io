de89a49f1584837a266f79968a2479487a5e4cc1
zergl <e3.gemini@qq.com>
2018-01-07 01:12:03 +0800

    [修改]1.双击修改页面背景颜色。

--- static/zergl.js
+++ static/zergl.js
@@ -1,1 +1,26 @@
 console.log("hello world!");
+
+g_bgcolor = "black";
+g_fontcolor = "green";
+
+function cur_bgcolor()
+{
+	bgcolor = document.body.style.background;
+	if (bgcolor == undefined || bgcolor == "")
+	{
+		bgcolor = "black";
+	}
+
+	return bgcolor;
+}
+window.onload = function()
+{
+	g_bgcolor = cur_bgcolor;
+}
+
+document.ondblclick=function ()
+{
+	bgcolor = cur_bgcolor();
+	change_color = bgcolor == "black" ? "white" : "black";
+	document.body.style.background = change_color;
+};
